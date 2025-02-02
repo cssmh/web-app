b5393f7442d2133b602f200dd474fccc24fb2560
Momen <touristmomen@gmail.com>
2025-02-02 17:47:11 +0600

    spinner updated

--- src/Shared/MainLayout.jsx
+++ src/Shared/MainLayout.jsx
@@ -10,7 +10,7 @@ const MainLayout = () => {
   useEffect(() => {
     const timer = setTimeout(() => {
       setIsLoading(false);
-    }, 1200);
+    }, 1100);
 
     return () => clearTimeout(timer);
   }, []);
